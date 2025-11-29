55137f9bc32f1ceba3fa3250b0cc1dec2f681df1
shinchan08113 <sonpur12345hajipur@gmail.com>
2025-11-29 20:09:15 +0530

    create moviesSlice  ,fetch the movies data from IMDB and make customeHook for making clean browsePage.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,17 @@
 export const LOGO =
   "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-08-26/consent/87b6a5c0-0104-4e96-a291-092c11350111/0198e689-25fa-7d64-bb49-0f7e75f898d2/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
-export const BANNER_IMAGE="https://assets.nflxext.com/ffe/siteui/vlv3/6fd9d446-cd78-453a-8c9c-417ed3e00422/web/IN-en-20251117-TRIFECTA-perspective_2fe4e381-977f-49fd-a7f4-1da0bcf09429_medium.jpg"  
+export const BANNER_IMAGE =
+  "https://assets.nflxext.com/ffe/siteui/vlv3/6fd9d446-cd78-453a-8c9c-417ed3e00422/web/IN-en-20251117-TRIFECTA-perspective_2fe4e381-977f-49fd-a7f4-1da0bcf09429_medium.jpg";
 
+export const USER_AVATAR =
+  "https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg";
 
-export const USER_AVATAR="https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg"
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyOWYzMjY0YTBiYjlmYzYzYzdmZjFmZWE5YjAzMmNjYSIsIm5iZiI6MTc2MTExMTI4MC4zMywic3ViIjoiNjhmODZjZjBhYTZjNDI4MDQ3ODA1NTg4Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.ICu3B4tv-RA97gVNNSYiMdqIwpIZgnSPwAxXDv9tLG0",
+  },
+};
